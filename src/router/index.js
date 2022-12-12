2ade26261824b6c40ced0193f392110308417889
菍磐 <qaz123cxd@yeah.net>
2022-12-12 23:07:47 +0800

    dev reportPage

--- src/router/index.js
+++ src/router/index.js
@@ -57,6 +57,10 @@ const routes = [
       {
         path: '/orders',
         component:() => import('../views/order/Order')
+      },
+      {
+        path: '/reports',
+        component:() => import('../views/report/Report')
       }
 
     ]
